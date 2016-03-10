e24f6d32a20a9283c86d62e5dbd420abeae213ee
Bitnetcom <berebitbet@gmail.com>
2016-03-10 19:08:13 +0100

    Initial Commit

---
+++ RetoNumeros/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RetoNumeros
+//
+//  Created by MacBook Air on 10/03/2016.
+//  Copyright © 2016 MacBook Air. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RetoNumeros/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RetoNumeros
+//
+//  Created by MacBook Air on 10/03/2016.
+//  Copyright © 2016 MacBook Air. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
